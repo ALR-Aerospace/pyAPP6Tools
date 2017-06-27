0c26d5592e05bbcdf6b77d058db82313192d6458
marc.immer <marc.immer@alr-aerospace.ch>
2017-06-27 09:09:52 +0200

    use mock instead of custom test_pyAPP6Files

--- tests/test_TableHelper.py
+++ tests/test_TableHelper.py
@@ -6,12 +6,15 @@ Created on Thu Jun 15 18:40:30 2017
 """
 #pylint: disable-msg=C0103
 import unittest
+import sys
 import mock
 
-
 import numpy as np
-import test_pyAPP6Files
 
+#use mock to removed missing dependency from TableHelper
+sys.modules['pyAPP6'] = mock.Mock()
+
+from pyAPP6Tools import TableHelper
 
 class TestTableHelper(unittest.TestCase):
 
@@ -19,25 +22,17 @@ class TestTableHelper(unittest.TestCase):
         pass
 
     def test_restructureArrayData_nonEqualArrays_shouldRaiseException(self):
-        with mock.patch.dict('sys.modules', **{'pyAPP6' : test_pyAPP6Files,
-                                'pyAPP6.Files' : test_pyAPP6Files}):
-            from pyAPP6Tools import TableHelper
-            
-            a = np.array([0.0, 0.0, 0.0])
-            b = np.array([0.0, 0.0, 0.0])
-            c = np.array([0.0, 0.0, 0.0])
-            d = np.array([0.0, 0.0])
-            with self.assertRaises(ValueError):
-                TableHelper.restructureArrayData(a, b, c, d)
+        a = np.array([0.0, 0.0, 0.0])
+        b = np.array([0.0, 0.0, 0.0])
+        c = np.array([0.0, 0.0, 0.0])
+        d = np.array([0.0, 0.0])
+        with self.assertRaises(ValueError):
+            TableHelper.restructureArrayData(a, b, c, d)
 
     def test_restructureArrayData_Length(self):
-        with mock.patch.dict('sys.modules', **{'pyAPP6' : test_pyAPP6Files,
-                                'pyAPP6.Files' : test_pyAPP6Files}):
-            from pyAPP6Tools import TableHelper
-            
-            a = np.array([0.0, 0.0, 0.0])
-            b = np.array([0.0, 0.0, 0.0])
-            c = np.array([1.0, 2.0, 3.0])
-            d = np.array([0.5, 0.7, 0.9])
-            res = TableHelper.restructureArrayData(a, b, c, d)
-            self.assertEqual(len(res), 1)
+        a = np.array([0.0, 0.0, 0.0])
+        b = np.array([0.0, 0.0, 0.0])
+        c = np.array([1.0, 2.0, 3.0])
+        d = np.array([0.5, 0.7, 0.9])
+        res = TableHelper.restructureArrayData(a, b, c, d)
+        self.assertEqual(len(res), 1)
